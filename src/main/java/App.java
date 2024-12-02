b7643f2cd0caee61a3b011dd7b08b270a6c7e740
Azat <kushkarov.azat@gmail.com>
2024-12-02 16:22:45 +0300

    solved task

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,11 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        HelloWorld firstBean = (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld secondBean = (HelloWorld) applicationContext.getBean("helloworld");
+        Cat firstCat = (Cat) applicationContext.getBean("cat");
+        Cat secondCat = (Cat) applicationContext.getBean("cat");
+        System.out.println(firstBean==secondBean);
+        System.out.println(firstCat==secondCat);
     }
 }
